e4bc07826889aa944922acf113c01f705ab046a5
Igor Jerosimic <64783856+igor-jerosimic-cko@users.noreply.github.com>
2025-05-13 22:31:41 +0100

    WIP

--- src/Moneybox.App.Test/WithdrawMoneyTests.cs
+++ src/Moneybox.App.Test/WithdrawMoneyTests.cs
@@ -8,107 +8,164 @@ namespace Moneybox.App.Test;
 
 public class TransferMoneyTests
 {
+    private readonly Guid _fromAccountId = Guid.NewGuid();
+    private readonly Guid _toAccountId = Guid.NewGuid();
+    private readonly Account _fromAccount;
+    private readonly Account _toAccount;
+    private readonly Mock<INotificationService> _mockNotificationService;
+    private readonly IAccountRepository _mockRepo;
+
+    public TransferMoneyTests()
+    {
+        _fromAccount = Account.Create(_fromAccountId, new User(), 10000);
+        _toAccount = Account.Create(_toAccountId, new User(), 500);
+
+        _mockRepo = Mock.Of<IAccountRepository>(repo =>
+            repo.GetAccountById(_fromAccountId) == _fromAccount &&
+            repo.GetAccountById(_toAccountId) == _toAccount);
+
+        _mockNotificationService = new Mock<INotificationService>();
+        _mockNotificationService
+            .Setup(service => service.NotifyFundsLow(It.IsAny<string>()))
+            .Verifiable();
+        _mockNotificationService
+            .Setup(service => service.NotifyApproachingPayInLimit(It.IsAny<string>()))
+            .Verifiable();
+    }
+
     [Fact]
     public void It_Transfers_Money()
     {
-        var fromAccountId = Guid.NewGuid();
-        var fromAccount = Account.Create(fromAccountId, new User(), 1000);
-        
-        var toAccountId = Guid.NewGuid();
-        var toAccount = Account.Create(toAccountId, new User(), 500);
+        var transferMoney = new TransferMoney(_mockRepo, _mockNotificationService.Object);
+        transferMoney.Execute(_fromAccountId, _toAccountId, amount: 100);
 
-        var mockRepo = Mock.Of<IAccountRepository>(repo =>
-            repo.GetAccountById(fromAccountId) == fromAccount &&
-            repo.GetAccountById(toAccountId) == toAccount);
+        _fromAccount.Balance.Should().Be(9900);
+        _fromAccount.Withdrawn.Should().Be(100);
         
-        var mockNotificationService = new Mock<INotificationService>();
-        mockNotificationService
-            .Setup(service => service.NotifyFundsLow(It.IsAny<string>()))
-            .Verifiable();
-        mockNotificationService
-            .Setup(service => service.NotifyApproachingPayInLimit(It.IsAny<string>()))
-            .Verifiable();
+        _fromAccount.Withdrawn.Should().Be(100);
+        _toAccount.PaidIn.Should().Be(100);
+
+        _mockNotificationService.Verify(service => service.NotifyFundsLow(It.IsAny<string>()), Times.Never);
+        _mockNotificationService.Verify(service => service.NotifyApproachingPayInLimit(It.IsAny<string>()), Times.Never);
+    }
+
+    [Fact]
+    public void It_Transfers_Money_And_Notifies_On_Low_Balance()
+    {
+        _fromAccount.Withdraw(9000);
+        _fromAccount.Withdrawn.Should().Be(9000);
+        _fromAccount.Balance.Should().Be(1000);
         
-        var from = mockRepo.GetAccountById(fromAccountId)!;
-        var to = mockRepo.GetAccountById(toAccountId)!;
+        var transferMoney = new TransferMoney(_mockRepo, _mockNotificationService.Object);
+        transferMoney.Execute(_fromAccountId, _toAccountId, amount: 800);
+
+        _fromAccount.Balance.Should().Be(200);
+        _toAccount.Balance.Should().Be(1300);
         
-        var transferMoney = new TransferMoney(mockRepo, mockNotificationService.Object);
-        transferMoney.Execute(fromAccountId, toAccountId, amount: 100);
+        _fromAccount.Withdrawn.Should().Be(9800);
+        _toAccount.PaidIn.Should().Be(800);
+
+        _mockNotificationService.Verify(service => service.NotifyFundsLow(It.IsAny<string>()), Times.Once);
+        _mockNotificationService.Verify(service => service.NotifyApproachingPayInLimit(It.IsAny<string>()), Times.Never);
+    }
+    
+    [Fact]
+    public void It_Transfers_Money_And_Notifies_On_Approaching_Pay_In_Limit()
+    {
+        const decimal amountNearPayInLimit = Account.PayInLimit - 100;
         
-        from.Balance.Should().Be(900);
-        to.Balance.Should().Be(600);
+        var transferMoney = new TransferMoney(_mockRepo, _mockNotificationService.Object);
+        transferMoney.Execute(_fromAccountId, _toAccountId, amountNearPayInLimit);
+
+        _fromAccount.Balance.Should().Be(6100);
+        _toAccount.Balance.Should().Be(4400);
         
-        mockNotificationService.Verify(service => service.NotifyFundsLow(It.IsAny<string>()), Times.Never);
-        mockNotificationService.Verify(service => service.NotifyApproachingPayInLimit(It.IsAny<string>()), Times.Never);
+        _fromAccount.Withdrawn.Should().Be(3900);
+        _toAccount.PaidIn.Should().Be(3900);
+
+        _mockNotificationService.Verify(service => service.NotifyFundsLow(It.IsAny<string>()), Times.Never);
+        _mockNotificationService.Verify(service => service.NotifyApproachingPayInLimit(It.IsAny<string>()), Times.Once);
     }
     
     [Fact]
     public void It_Does_Not_Transfer_Money_When_From_Balance_Is_Too_Low()
     {
-        var fromAccountId = Guid.NewGuid();
-        var fromAccount = Account.Create(fromAccountId, new User(), 1000);
+        _fromAccount.Withdraw(9000);
+        _fromAccount.Withdrawn.Should().Be(9000);
+        _fromAccount.Balance.Should().Be(1000);
         
-        var toAccountId = Guid.NewGuid();
-        var toAccount = Account.Create(toAccountId, new User(), 500);
-
-        var mockRepo = Mock.Of<IAccountRepository>(repo =>
-            repo.GetAccountById(fromAccountId) == fromAccount &&
-            repo.GetAccountById(toAccountId) == toAccount);
+        var amountGreaterThanBalance = _fromAccount.Balance + 500;
         
-        var mockNotificationService = new Mock<INotificationService>();
-        mockNotificationService
-            .Setup(service => service.NotifyFundsLow(It.IsAny<string>()))
-            .Verifiable();
-        mockNotificationService
-            .Setup(service => service.NotifyApproachingPayInLimit(It.IsAny<string>()))
-            .Verifiable();
-        
-        var from = mockRepo.GetAccountById(fromAccountId)!;
-        var to = mockRepo.GetAccountById(toAccountId)!;
-        
-        var transferMoney = new TransferMoney(mockRepo, mockNotificationService.Object);
-
-        var act = () => transferMoney.Execute(fromAccountId, toAccountId, amount: 2000);
+        var transferMoney = new TransferMoney(_mockRepo, _mockNotificationService.Object);
+        var act = () => transferMoney.Execute(_fromAccountId, _toAccountId, amountGreaterThanBalance);
         act.Should().Throw<InvalidOperationException>().WithMessage("Insufficient funds to make transfer");
+
+        _fromAccount.Balance.Should().Be(1000);
+        _toAccount.Balance.Should().Be(500);
         
-        from.Balance.Should().Be(1000);
-        to.Balance.Should().Be(500);
-        
-        mockNotificationService.Verify(service => service.NotifyFundsLow(It.IsAny<string>()), Times.Never);
-        mockNotificationService.Verify(service => service.NotifyApproachingPayInLimit(It.IsAny<string>()), Times.Never);
+        _fromAccount.Withdrawn.Should().Be(9000);
+        _toAccount.PaidIn.Should().Be(0);
+
+        _mockNotificationService.Verify(service => service.NotifyFundsLow(It.IsAny<string>()), Times.Never);
+        _mockNotificationService.Verify(service => service.NotifyApproachingPayInLimit(It.IsAny<string>()), Times.Never);
     }
     
     [Fact]
-    public void It_Transfers_Money_And_Notifies_On_Low_Balance()
-    {
-        var fromAccountId = Guid.NewGuid();
-        var fromAccount = Account.Create(fromAccountId, new User(), 1000);
+    public void It_Does_Not_Transfer_Money_When_Pay_In_Limit_Reached()
+    { 
+        const decimal amountGreaterThanLimit = Account.PayInLimit + 1000;
         
-        var toAccountId = Guid.NewGuid();
-        var toAccount = Account.Create(toAccountId, new User(), 500);
+        var transferMoney = new TransferMoney(_mockRepo, _mockNotificationService.Object);
+        var act = () => transferMoney.Execute(_fromAccountId, _toAccountId, amountGreaterThanLimit);
+        act.Should().Throw<InvalidOperationException>().WithMessage("Account pay in limit reached");
 
-        var mockRepo = Mock.Of<IAccountRepository>(repo =>
-            repo.GetAccountById(fromAccountId) == fromAccount &&
-            repo.GetAccountById(toAccountId) == toAccount);
+        _fromAccount.Balance.Should().Be(10000);
+        _toAccount.Balance.Should().Be(500);
         
-        var mockNotificationService = new Mock<INotificationService>();
-        mockNotificationService
-            .Setup(service => service.NotifyFundsLow(It.IsAny<string>()))
-            .Verifiable();
-        mockNotificationService
-            .Setup(service => service.NotifyApproachingPayInLimit(It.IsAny<string>()))
-            .Verifiable();
+        _fromAccount.Withdrawn.Should().Be(0);
+        _toAccount.PaidIn.Should().Be(0);
+
+        _mockNotificationService.Verify(service => service.NotifyFundsLow(It.IsAny<string>()), Times.Never);
+        _mockNotificationService.Verify(service => service.NotifyApproachingPayInLimit(It.IsAny<string>()), Times.Never);
+    }
+    
+    [Fact]
+    public void It_Transfers_Money_In_Multiple_Transactions_Until_Pay_In_Limit_Reached()
+    { 
+        var transferMoney = new TransferMoney(_mockRepo, _mockNotificationService.Object);
+    
+        transferMoney.Execute(_fromAccountId, _toAccountId, 1500);
         
-        var from = mockRepo.GetAccountById(fromAccountId)!;
-        var to = mockRepo.GetAccountById(toAccountId)!;
+        _fromAccount.Balance.Should().Be(8500);
+        _toAccount.Balance.Should().Be(2000);
+
+        _fromAccount.Withdrawn.Should().Be(1500);
+        _toAccount.PaidIn.Should().Be(1500);
         
-        var transferMoney = new TransferMoney(mockRepo, mockNotificationService.Object);
-        transferMoney.Execute(fromAccountId, toAccountId, amount: 800);
+        _mockNotificationService.Verify(service => service.NotifyFundsLow(It.IsAny<string>()), Times.Never);
+        _mockNotificationService.Verify(service => service.NotifyApproachingPayInLimit(It.IsAny<string>()), Times.Never);
         
-        from.Balance.Should().Be(200);
-        to.Balance.Should().Be(1300);
+        transferMoney.Execute(_fromAccountId, _toAccountId, 2500);
         
-        mockNotificationService.Verify(service => service.NotifyFundsLow(It.IsAny<string>()), Times.Once);
-        mockNotificationService.Verify(service => service.NotifyApproachingPayInLimit(It.IsAny<string>()), Times.Never);
+        _fromAccount.Balance.Should().Be(6000);
+        _toAccount.Balance.Should().Be(4500);
+        
+        _fromAccount.Withdrawn.Should().Be(4000);
+        _toAccount.PaidIn.Should().Be(4000);
+        
+        _mockNotificationService.Verify(service => service.NotifyFundsLow(It.IsAny<string>()), Times.Never);
+        _mockNotificationService.Verify(service => service.NotifyApproachingPayInLimit(It.IsAny<string>()), Times.Once);
+
+        var act = () =>  transferMoney.Execute(_fromAccountId, _toAccountId, 1500);
+        act.Should().Throw<InvalidOperationException>().WithMessage("Account pay in limit reached");
+
+        _fromAccount.Balance.Should().Be(6000);
+        _toAccount.Balance.Should().Be(4500);
+        
+        _fromAccount.Withdrawn.Should().Be(4000);
+        _toAccount.PaidIn.Should().Be(4000);
+
+        _mockNotificationService.Verify(service => service.NotifyFundsLow(It.IsAny<string>()), Times.Never);
+        _mockNotificationService.Verify(service => service.NotifyApproachingPayInLimit(It.IsAny<string>()), Times.Once);
     }
 }
